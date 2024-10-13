e9cab2433543ba2bdf2c8978c03beb19314bd0f0
denisinside <d.shvachka@ukma.edu.ua>
2024-10-13 19:50:20 +0300

    move_semantics   done

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -8,10 +8,13 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
+// I AM DONE
 
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(n: i32) -> i32 {
+    let price = if n > 40 { 1  } else { 2 };
+    n*price
+}
 
 // Don't modify this function!
 #[test]
