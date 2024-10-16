2ae9cf37d06ca4c72539d5e1af222d673b18bd29
denisinside <d.shvachka@ukma.edu.ua>
2024-10-16 17:48:51 +0300

    iterators done

--- rustlings/exercises/standard_library_types/iterators4.rs
+++ rustlings/exercises/standard_library_types/iterators4.rs
@@ -1,6 +1,6 @@
 // iterators4.rs
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -12,12 +12,18 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    (1..=num).product()
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
+    fn factorial_of_0() {
+        assert_eq!(1, factorial(0));
+    }
     #[test]
     fn factorial_of_1() {
         assert_eq!(1, factorial(1));
